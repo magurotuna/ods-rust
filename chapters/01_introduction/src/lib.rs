a49c03091cc6b307c523469caeb3fc31c7a12b1e
Yusuke Tanaka <wing0920@gmail.com>
2020-04-04 16:30:42 +0900

    q1_3

--- chapters/01_introduction/src/lib.rs
+++ chapters/01_introduction/src/lib.rs
@@ -1,2 +1,3 @@
 mod q1_1;
 mod q1_2;
+mod q1_3;
---
+++ chapters/01_introduction/src/q1_3.rs
@@ -0,0 +1,52 @@
+use std::convert::{TryFrom, TryInto};
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Parenthesis {
+    Paren,
+    Brace,
+    Bracket,
+}
+
+impl TryFrom<char> for Parenthesis {
+    type Error = &'static str;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            '(' | ')' => Ok(Parenthesis::Paren),
+            '{' | '}' => Ok(Parenthesis::Brace),
+            '[' | ']' => Ok(Parenthesis::Bracket),
+            _ => Err("Cannot convert this character to a parenthesis."),
+        }
+    }
+}
+
+fn is_match_string(s: &str) -> bool {
+    let mut stack = Vec::new();
+    for c in s.chars() {
+        let p: Parenthesis = c.try_into().unwrap();
+        match stack.pop() {
+            Some(pop) => {
+                if pop != p {
+                    return false;
+                }
+            }
+            None => stack.push(p),
+        }
+    }
+    true
+}
+
+fn main() {
+    ()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_match_string() {
+        assert!(is_match_string("{{()[]}}"));
+        assert!(!is_match_string("{{()]}"));
+    }
+}
